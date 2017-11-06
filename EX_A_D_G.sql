7848bdcd93f5091d846ea1058858526a57702000
Guilherme Ferreira <1150623@isep.ipp.pt>
2017-11-06 18:11:44 +0000

    Alteração minima bootstrap + Ex A e D

--- EX_A_D_G.sql
+++ EX_A_D_G.sql
@@ -1,7 +1,7 @@
 
 --+-------------------+------------------------------------------------------------------------------------------------+
 --+-------------------+                                                                                               -+
---+-- Exercício (a) --+Liste as autoestradas que se cruzam com a A4, indicando o km onde elas se cruzam.              -+
+--+-- Exercício (a) --+ Liste as autoestradas que se cruzam com a A4, indicando o km onde elas se cruzam.             -+
 --+-------------------+                                                                                               -+
 --+-------------------+------------------------------------------------------------------------------------------------+
 
@@ -13,14 +13,53 @@ select lig.codautoestrada1,lig.QUILOMETRO1, lig.CODAUTOESTRADA2, lig.QUILOMETRO2
 --+-------------------+                                                                                               -+
 --+-------------------+------------------------------------------------------------------------------------------------+
 
-select pp.MATRICULAVEICULO from PASSAGEMPORTICO pp where extract(year from pp.DATAPASSAGEM) <> 2016; 
 
-
-select c.NOME, c.NIF, d.MATRICULAVEICULO from Dispositivo d, Cliente c where d.CLIENTENIF=c.NIF and d.MATRICULAVEICULO not in(select pp.MATRICULAVEICULO from PASSAGEMPORTICO pp where extract(year from pp.DATAPASSAGEM) <> 2016 );
+select c.NOME, c.NIF, d.MATRICULAVEICULO from Dispositivo d, Cliente c where d.CLIENTENIF=c.NIF and d.MATRICULAVEICULO in (select pp.MATRICULAVEICULO from PASSAGEMPORTICO pp where extract(year from pp.DATAPASSAGEM) = 2016)
+                                                                                                                            and d.MATRICULAVEICULO not in (select re.MATRICULAVEICULO from RegistoEntrada re where extract(year from re.DATAREG) = 2016);
 
 
 --+-------------------+------------------------------------------------------------------------------------------------+
---+-------------------+ Liste os clientes que somente circularam em autoestradas com pórticos durante o ano de 2016.  -+
---+-- Exercício (g) --+ Um cliente é identificado pelo NIF e pode ter vários veículos (dispositivos).                 -+
---+-------------------+                                                                                               -+
+--+-------------------+ Indique qual a autoestrada, com o tipo de portagem tradicional, que obteve o maior número de  -+
+--+-- Exercício (g) --+ passagens com o dispositivo inativo, do que qualquer autoestrada com  portagens do tipo       -+
+--+-------------------+ pórticos, entre o período de 01/01/2017 a 10/10/2017.                                         -+
 --+-------------------+------------------------------------------------------------------------------------------------+
+
+select SUM(COUNT(*)) from Portagemtradicional pt, Dispositivo d, RegistoEntrada re where
+                                                                                --Joins
+                                                                                pt.CODAUTOESTRADA = ae.CodAUTOESTRADA
+                                                                                and pt.CODPORTTRADICIONAL = re.CODPORTTRADICIONAL and pt.CODAUTOESTRADA = re.CODAUTOESTRADA and d.MATRICULAVEICULO = re.MATRICULAVEICULO
+                                                                                --Restrição Dispositivo Inativo
+                                                                                and d.ESTADODISPOSITIVO = 0 
+                                                                                --Restrição Ano
+                                                                                and extract(year from re.DATAREG) = 2017 
+                                                                                --Restrição Dia e Mês
+                                                                                and (extract(month from re.DATAREG) < 10 or (extract(month from re.DATAREG) = 10 and extract(day from re.DATAREG) < 10));
+                              
+
+
+select ae.codautoestrada from Autoestrada ae, AutoEstrada ae1 where (select Count(*) from Portagemtradicional pt, Dispositivo d, RegistoEntrada re where
+                                                                                --Joins
+                                                                                pt.CODAUTOESTRADA = ae.CodAUTOESTRADA
+                                                                                and pt.CODPORTTRADICIONAL = re.CODPORTTRADICIONAL and pt.CODAUTOESTRADA = re.CODAUTOESTRADA and d.MATRICULAVEICULO = re.MATRICULAVEICULO
+                                                                                --Restrição Dispositivo Inativo
+                                                                                and d.ESTADODISPOSITIVO = 0 
+                                                                                --Restrição Ano
+                                                                                and extract(year from re.DATAREG) = 2017 
+                                                                                --Restrição Dia e Mês
+                                                                                and (extract(month from re.DATAREG) < 10 or (extract(month from re.DATAREG) = 10 and extract(day from re.DATAREG) < 10)) group by pt.CODAUTOESTRADA having soma = SUM(COUNT(*))) 
+                                                                                
+                                                                                > 
+                                                                                
+                                                                                
+                                                                                (select SUM(COUNT(*)) from Portagemtradicional pt, Dispositivo d, RegistoEntrada re where
+                                                                                --Joins
+                                                                                pt.CODAUTOESTRADA = ae1.CodAUTOESTRADA
+                                                                                and pt.CODPORTTRADICIONAL = re.CODPORTTRADICIONAL and pt.CODAUTOESTRADA = re.CODAUTOESTRADA and d.MATRICULAVEICULO = re.MATRICULAVEICULO
+                                                                                --Restrição Dispositivo Inativo
+                                                                                and d.ESTADODISPOSITIVO = 0 
+                                                                                --Restrição Ano
+                                                                                and extract(year from re.DATAREG) = 2017 
+                                                                                --Restrição Dia e Mês
+                                                                                and (extract(month from re.DATAREG) < 10 or (extract(month from re.DATAREG) = 10 and extract(day from re.DATAREG) < 10))group by pt.CODAUTOESTRADA having soma = SUM(COUNT(*)));
+                              
+
